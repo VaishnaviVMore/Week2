1000853244eebbf2ad532736199384ee2d2d0531
VaishnaviVMore <07mvaishnavi@gmail.com>
2024-09-30 23:06:25 +0530

    Trim function use

---
+++ Trim.java
@@ -0,0 +1,7 @@
+public class Trim {
+    public static void main(String[] args) {
+        String str="                             The world is full of joy";
+        System.out.println(str);
+        System.out.println(str.trim());
+    }
+}
